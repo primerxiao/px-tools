ad5c56be55bb3f6577e6d897e3c9563624267fa6
primerxiao <primerxiao@163.com>
2019-06-18 23:41:12 +0800

    是

--- src/main/java/pxt/entity/MenuItem.java
+++ src/main/java/pxt/entity/MenuItem.java
@@ -9,14 +9,42 @@ import lombok.Data;
 @Data
 public class MenuItem {
 
+  /**
+   * 菜单id
+   */
   private int menuId;
+  /**
+   * 菜单类型
+   */
   private String menuType;
+  /**
+   * 菜单显示文本
+   */
   private String menuText;
+  /**
+   * 父菜单id
+   */
   private int menuParentId;
+
+  /**
+   * 排序
+   */
   private int menuIndex;
+  /**
+   * 菜单图标
+   */
   private String menuIcon;
+  /**
+   *  菜单图标大小
+   */
   private int menuIconSize;
+  /**
+   * 菜单样式
+   */
   private String menuStyleClass;
+  /**
+   * 菜单组
+   */
   private int menuGroup;
 
 }
